8cf7a16dbb452b5b5ec027363f5649748ab98432
Kirsten Korevaar <kirsten@eml.cc>
2019-01-16 17:54:37 +0100

    vide with user input

--- catch-of-the-day/src/components/Inventory.js
+++ catch-of-the-day/src/components/Inventory.js
@@ -1,11 +1,19 @@
 import React from "react";
 import AddFishForm from "./AddFishForm";
+import EditFishForm from "./EditFishForm";
 
 class Inventory extends React.Component {
   render() {
     return (
       <div className="inventory">
         <h2>Inventory</h2>
+        {Object.keys(this.props.fish).map(fish => 
+        <EditFishForm 
+        key={fish} 
+        index={fish}
+        fish={this.props.fish[fish]} 
+        updateFish={this.props.updateFish}/>
+        )}
         <AddFishForm addFish={this.props.addFish} />
         <button onClick={this.props.loadSampleFishes}>Load sample Fishes</button>
       </div>
